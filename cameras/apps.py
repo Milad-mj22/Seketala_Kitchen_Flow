a37c5efea7cc196ee74cae59f6132542d956fda6
milad <m.moltaji@yahoo.com>
2025-12-01 17:24:08 +0330

    add notif for add customer

--- cameras/apps.py
+++ cameras/apps.py
@@ -2,23 +2,23 @@ from django.apps import AppConfig
 
 
 
-# apps.py
-from django.apps import AppConfig
-from cameras.services import CameraManager
+# # apps.py
+# from django.apps import AppConfig
+# from cameras.services import CameraManager
 
-class CamerasConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cameras'
+# class CamerasConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'cameras'
 
-    def ready(self):
-        from django.conf import settings
-        try:
-            from cameras.models import Camera
+#     def ready(self):
+#         from django.conf import settings
+#         try:
+#             from cameras.models import Camera
 
-            cameras = Camera.objects.all()
-            for cam in cameras:
-                CameraManager.get_camera(str(cam.id), cam.get_live_feed_url())
-        except Exception as e:
-            #print(e)
-            pass  # migrations stage, DB not ready
+#             cameras = Camera.objects.all()
+#             for cam in cameras:
+#                 CameraManager.get_camera(str(cam.id), cam.get_live_feed_url())
+#         except Exception as e:
+#             #print(e)
+#             pass  # migrations stage, DB not ready
 
