bcccacf88ed9f7cfcd89d6f06372a8ec9da100f4
milad <m.moltaji@yahoo.com>
2026-02-07 11:10:36 +0330

    modify days

--- DataAnalysis/urls.py
+++ DataAnalysis/urls.py
@@ -12,6 +12,12 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('nahve/', views.calc_nahve_pardakh, name='calc_nahve_pardakht'),
     path("api/receive-invoice/", views.ReceiveInvoice.as_view()),
-
-    
+    path("report/", views.invoice_report, name="invoice_report"),
+    path("report/download/", views.download_invoice_excel, name="download_invoice_excel"),
+    path("report/download-summary/", views.download_invoice_summary_excel, name="download_invoice_summary_excel"),
+    path(
+        "api/invoices/<str:invoice_number>/",
+        views.invoice_detail_api,
+        name="invoice_detail_api"
+    ),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
